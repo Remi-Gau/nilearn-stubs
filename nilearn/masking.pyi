222c203cfc9639dd34e777dbd635ef979e71e97e
Remi Gau <remi_gau@hotmail.com>
2024-12-31 12:32:35 +0100

    set up

--- nilearn/masking.pyi
+++ nilearn/masking.pyi
@@ -1,3 +1,7 @@
+from typing import (
+    Any,
+)
+
 from joblib.memory import Memory
 from nibabel.nifti1 import Nifti1Image
 from numpy import (
@@ -8,13 +12,6 @@ from numpy.dtypes import (
     Float32DType,
     Float64DType,
     Int32DType,
-)
-from typing import (
-    Any,
-    List,
-    Optional,
-    Tuple,
-    Union,
 )
 
 __all__ = [
@@ -29,59 +26,47 @@ __all__ = [
     "unmask",
 ]
 
-
 def apply_mask(
-    imgs: Union[str, Nifti1Image],
-    mask_img: Union[str, Nifti1Image],
-    dtype: Union[Float64DType, Float32DType, Int32DType, str] = ...,
-    smoothing_fwhm: Optional[int] = ...,
-    ensure_finite: bool = ...
+    imgs: str | Nifti1Image,
+    mask_img: str | Nifti1Image,
+    dtype: Float64DType | Float32DType | Int32DType | str = ...,
+    smoothing_fwhm: int | None = ...,
+    ensure_finite: bool = ...,
 ) -> ndarray: ...
-
-
-
-
-
 def compute_background_mask(
-    data_imgs: Union[memmap, ndarray, List[Nifti1Image], Nifti1Image],
+    data_imgs: memmap | ndarray | list[Nifti1Image] | Nifti1Image,
     border_size: int = ...,
     connected: bool = ...,
-    opening: Union[int, bool] = ...,
+    opening: int | bool = ...,
     target_affine: None = ...,
     target_shape: None = ...,
-    memory: Optional[Memory] = ...,
-    verbose: int = ...
+    memory: Memory | None = ...,
+    verbose: int = ...,
 ) -> Nifti1Image: ...
-
-
 def compute_brain_mask(
     target_img: Nifti1Image,
-    threshold: Union[int, float] = ...,
+    threshold: int | float = ...,
     connected: bool = ...,
     opening: int = ...,
-    memory: Optional[Memory] = ...,
+    memory: Memory | None = ...,
     verbose: int = ...,
-    mask_type: str = ...
+    mask_type: str = ...,
 ) -> Nifti1Image: ...
-
-
 def compute_epi_mask(
     epi_img: Nifti1Image,
     lower_cutoff: float = ...,
     upper_cutoff: float = ...,
     connected: bool = ...,
-    opening: Union[int, bool] = ...,
+    opening: int | bool = ...,
     exclude_zeros: bool = ...,
     ensure_finite: bool = ...,
-    target_affine: Optional[ndarray] = ...,
-    target_shape: Optional[Tuple[int, int, int]] = ...,
+    target_affine: ndarray | None = ...,
+    target_shape: tuple[int, int, int] | None = ...,
     memory: None = ...,
-    verbose: int = ...
+    verbose: int = ...,
 ) -> Nifti1Image: ...
-
-
 def compute_multi_background_mask(
-    data_imgs: Union[memmap, List[Nifti1Image], ndarray, List[List[Nifti1Image]]],
+    data_imgs: memmap | list[Nifti1Image] | ndarray | list[list[Nifti1Image]],
     border_size: int = ...,
     connected: bool = ...,
     opening: int = ...,
@@ -89,48 +74,40 @@ def compute_multi_background_mask(
     target_affine: None = ...,
     target_shape: None = ...,
     n_jobs: int = ...,
-    memory: Optional[Memory] = ...,
-    verbose: int = ...
+    memory: Memory | None = ...,
+    verbose: int = ...,
 ) -> Nifti1Image: ...
-
-
 def compute_multi_brain_mask(
-    target_imgs: List[Union[Any, Nifti1Image]],
+    target_imgs: list[Any | Nifti1Image],
     threshold: float = ...,
     connected: bool = ...,
     opening: int = ...,
-    memory: Optional[Memory] = ...,
+    memory: Memory | None = ...,
     verbose: int = ...,
     mask_type: str = ...,
-    **kwargs
+    **kwargs,
 ) -> Nifti1Image: ...
-
-
 def compute_multi_epi_mask(
-    epi_imgs: List[Union[Any, Nifti1Image]],
+    epi_imgs: list[Any | Nifti1Image],
     lower_cutoff: float = ...,
     upper_cutoff: float = ...,
     connected: bool = ...,
     opening: int = ...,
     threshold: float = ...,
-    target_affine: Optional[ndarray] = ...,
-    target_shape: Optional[Tuple[int, int, int]] = ...,
+    target_affine: ndarray | None = ...,
+    target_shape: tuple[int, int, int] | None = ...,
     exclude_zeros: bool = ...,
     n_jobs: int = ...,
     memory: None = ...,
-    verbose: int = ...
+    verbose: int = ...,
 ) -> Nifti1Image: ...
-
-
 def intersect_masks(
-    mask_imgs: List[Union[str, Nifti1Image]],
+    mask_imgs: list[str | Nifti1Image],
     threshold: float = ...,
-    connected: bool = ...
+    connected: bool = ...,
 ) -> Nifti1Image: ...
-
-
 def unmask(
-    X: Union[List[bool], List[ndarray], ndarray],
-    mask_img: Union[str, Nifti1Image],
-    order: str = ...
-) -> Union[List[Nifti1Image], Nifti1Image]: ...
+    X: list[bool] | list[ndarray] | ndarray,
+    mask_img: str | Nifti1Image,
+    order: str = ...,
+) -> list[Nifti1Image] | Nifti1Image: ...
--- nilearn/signal.pyi
+++ nilearn/signal.pyi
@@ -1,16 +1,11 @@
+from typing import (
+    Any,
+)
+
 from numpy import (
     float64,
     memmap,
     ndarray,
-)
-from pandas.core.frame import DataFrame
-from pathlib import PosixPath
-from typing import (
-    Any,
-    List,
-    Optional,
-    Tuple,
-    Union,
 )
 
 __all__ = [
@@ -21,37 +16,33 @@ __all__ = [
 
 def butterworth(
     signals: ndarray,
-    sampling_rate: Union[float, float64, int],
-    low_pass: Optional[Union[float, float64, int]] = ...,
-    high_pass: Optional[Union[float, float64, int]] = ...,
+    sampling_rate: float | float64 | int,
+    low_pass: float | float64 | int | None = ...,
+    high_pass: float | float64 | int | None = ...,
     order: int = ...,
-    padtype: Optional[str] = ...,
-    padlen: Optional[int] = ...,
-    copy: bool = ...
+    padtype: str | None = ...,
+    padlen: int | None = ...,
+    copy: bool = ...,
 ) -> ndarray: ...
-
-
 def clean(
-    signals: Union[memmap, List[List[float]], ndarray],
-    runs: Optional[ndarray] = ...,
-    detrend: Optional[bool] = ...,
-    standardize: Union[bool, str] = ...,
-    sample_mask: Optional[Union[str, List[ndarray], ndarray]] = ...,
-    confounds: Optional[Any] = ...,
+    signals: memmap | list[list[float]] | ndarray,
+    runs: ndarray | None = ...,
+    detrend: bool | None = ...,
+    standardize: bool | str = ...,
+    sample_mask: str | list[ndarray] | ndarray | None = ...,
+    confounds: Any | None = ...,
     standardize_confounds: bool = ...,
-    filter: Union[bool, str] = ...,
-    low_pass: Optional[Union[float, bool, float64]] = ...,
-    high_pass: Optional[Union[float64, float, int, bool]] = ...,
-    t_r: Optional[Union[float, float64, int]] = ...,
-    ensure_finite: Optional[bool] = ...,
+    filter: bool | str = ...,
+    low_pass: float | bool | float64 | None = ...,
+    high_pass: float64 | float | int | bool | None = ...,
+    t_r: float | float64 | int | None = ...,
+    ensure_finite: bool | None = ...,
     extrapolate: bool = ...,
-    **kwargs
-) -> Union[memmap, ndarray]: ...
-
-
+    **kwargs,
+) -> memmap | ndarray: ...
 def high_variance_confounds(
     series: ndarray,
     n_confounds: int = ...,
     percentile: float = ...,
-    detrend: bool = ...
+    detrend: bool = ...,
 ) -> ndarray: ...
